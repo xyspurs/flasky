1970e725fe441e209535b229cbb9c4449e42a745
xiaoyu <yuxiao2113@gmail.com>
2018-03-31 11:42:00 +0800

    Add flask script module

--- hello.py
+++ hello.py
@@ -3,10 +3,12 @@
 from flask import Flask
 from flask import request  
 from flask import make_response, redirect
-
+from flask_script import Manager
 
 
 app = Flask(__name__)  ## __name__ 比较多的用于__main__,程序主模块或者包名字, 直接启动当前.py脚本时，则__name__==__main__，否则等于__name__==模块名字
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
@@ -29,6 +31,6 @@ def gotobaidu():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
 
 

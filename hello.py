567094249d463ea77643398078a571cf4197acd1
xiaoyu <yuxiao2113@gmail.com>
2018-03-31 11:22:15 +0800

    Add new route function

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 from flask import Flask
+from flask import request  
 
 
 app = Flask(__name__)  ## __name__ 比较多的用于__main__,程序主模块或者包名字, 直接启动当前.py脚本时，则__name__==__main__，否则等于__name__==模块名字
@@ -8,6 +9,11 @@ app = Flask(__name__)  ## __name__ 比较多的用于__main__,程序主模块或
 @app.route('/')
 def index():
     return '<h1>It\'s XiaoYu World!</h1>'
+
+@app.route('/ug')
+def acquire_ug():
+    user_agent=request.headers.get("User-Agent")  
+    return "<p> Your browser is %s</p>" % user_agent
 
 
 if __name__ == "__main__":
